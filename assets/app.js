3492a138235a70185c92206da52c576c1cdad531
Sahar <sahar.dellouz@gmail.com>
2025-09-07 23:31:45 +0200

    registre et login

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,5 @@
 import './bootstrap.js';
+import "bootstrap/dist/css/bootstrap.min.css";
 /*
  * Welcome to your app's main JavaScript file!
  *
@@ -7,4 +8,3 @@ import './bootstrap.js';
  */
 import './styles/app.css';
 
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
