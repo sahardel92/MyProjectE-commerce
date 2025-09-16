ad509de18550735e2d1049cd176a54650bca18bc
Sahar <sahar.dellouz@gmail.com>
2025-09-16 21:39:25 +0200

    Ajout modification Ajoute

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Category;
+use App\Form\CategoryFormType;
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;   // <= la SEULE Request
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class CategoryController extends AbstractController
+{
+    #[Route('/category', name: 'app_category')]
+    public function index(CategoryRepository $categoryRepository): Response
+
+    {
+
+
+        $categories = $categoryRepository->findAll();
+
+
+        return $this->render('category/index.html.twig', [
+            'categories' => $categories
+        ]);
+    }
+
+    #[Route('/category/new', name: 'app_category_new')]
+    public function addCategory(EntityManagerInterface $entityManager, Request $request): Response
+    {
+        $category = new Category();
+
+
+        $form = $this->createForm(CategoryFormType::class, $category);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+           $entityManager->persist($category);
+           $entityManager->flush();
+        }
+
+
+        return $this->render('category/new.html.twig',['form'=>$form->createView()]);
+    }
+
+
+    #[Route('/category/{id}/update', name: 'app_category_update')]
+    public function update(Category $category, EntityManagerInterface $entityManager, Request $request):Response
+    {
+        $form = $this->createForm(CategoryFormType::class, $category);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $entityManager->flush();
+        }
+        
+        return $this->render('category/update.html.twig',['form'=>$form->createView()]);
+
+
+
+    }
+}
