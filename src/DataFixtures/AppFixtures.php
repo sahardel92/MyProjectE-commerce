362f2f7ac3de8333718c9c2c94fcb0bc92baec14
xana <idrisskapan53@gmail.com>
2025-09-29 21:43:11 +0100

    default admin user for the app

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,37 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
 
 class AppFixtures extends Fixture
 {
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher) {
+        $this->passwordHasher = $passwordHasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $admin = new User();
+        $admin->setEmail("admin@admin.com");
+        $admin->setPassword($this->hashPassword($admin, "admin"));
+        $admin->setRoles(["ROLE_ADMIN", "ROLE_EDITOR"]);
+        $admin->setFirstName("admin");
+        $admin->setLastName("admin");
 
+        $manager->persist($admin);
         $manager->flush();
+    }
+
+    public function hashPassword(
+        User $user,
+        string $plainPassword
+    ) {
+        return $this->passwordHasher->hashPassword($user, $plainPassword);
     }
 }
