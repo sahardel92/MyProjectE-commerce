3f5bcac7d8d11fb3e5585db7b2a6bd6da27f4eb6
Sahar <sahar.dellouz@gmail.com>
2025-09-29 21:33:10 +0200

    Ajout entité Order avec formulaire checkout les information des commande adresse client des cordonnée

---
+++ src/Form/OrderType.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\City;
+use App\Entity\Order;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class OrderType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstName', null, [
+                'attr' => [
+                    'class' => 'form form-control'
+                ]
+            ])
+            ->add('lastName', null, [
+                'attr' => [
+                    'class' => 'form form-control'
+                ]
+            ])
+            ->add('phone', null, [
+                'attr' => [
+                    'class' => 'form form-control'
+                ]
+            ])
+            ->add('adresse', null, [
+                'attr' => [
+                    'class' => 'form form-control'
+                ]
+            ])
+           
+            ->add('city', EntityType::class, [
+                'class' => City::class,
+                'choice_label' => 'name', // ayjbad bsmiya madina'id'
+                'attr' => [
+                    'class' => 'form-select'
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Order::class,
+        ]);
+    }
+}
