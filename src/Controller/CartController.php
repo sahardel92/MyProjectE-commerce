43eec084a009c74d09482ee6c267d7c5a9a387bd
Sahar <sahar.dellouz@gmail.com>
2025-09-29 14:05:42 +0200

    Ajout CartController : gestion panier (ajout produit, total avec array_sum)

---
+++ src/Controller/CartController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Attribute\Route;
+
+
+
+final class CartController extends AbstractController
+{
+        private $pr;
+       public function __construct(private readonly ProductRepository $productRepository)
+        {
+            $this->pr = $productRepository;
+        }
+
+    #[Route('/cart', name: 'app_cart', methods: ['GET'] )]
+    public function index(SessionInterface $session): Response
+    {
+        $cart = $session->get('cart', []);
+        $cartWithData = [];
+        foreach ($cart as $id => $quantity) {
+            $cartWithData[] = [
+                'product' => $this->productRepository->find($id),
+                'quantity' => $quantity
+            ];
+
+        }
+        $total = array_sum(array_map(function($item) {
+            return $item['product']->getPrice() * $item['quantity'];
+        }, $cartWithData));
+
+
+            return $this->render('cart/index.html.twig', [
+            'items' => $cartWithData,
+            'total' => $total,
+            dd($cartWithData)
+            
+    ]);
+    }
+
+    #[Route('/cart/add/{id}', name: 'app_cart_new', methods: ['GET'] )]
+
+    public function addToCart(int $id, SessionInterface $session):Response
+    {
+
+        $cart = $session->get('cart', []);
+        if (!empty($cart[$id])) {
+            $cart[$id]++;
+        } else {
+            $cart[$id] = 1;
+        }
+        $session->set('cart', $cart);
+
+        return $this->redirectToRoute('app_cart');
+
+    }
+
+
+
+}
