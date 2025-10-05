69166135a46d51fca71fa6377a897aa9e08d5f6f
xana <idrisskapan53@gmail.com>
2025-10-05 17:47:13 +0100

    zdna comments f l file li kaydir new admin f lwl

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -10,25 +10,34 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    // object zdnha bach ndiro hash l password
     private UserPasswordHasherInterface $passwordHasher;
 
     public function __construct(UserPasswordHasherInterface $passwordHasher) {
+        // dependency injection dyal passsword hasher
+        // ya3ni khchina no3 dyal l hasher f l class dyalna
         $this->passwordHasher = $passwordHasher;
     }
 
     public function load(ObjectManager $manager): void
     {
+        // fixture bach n9ado new admin user f lwl dyal base de donnee
         $admin = new User();
         $admin->setEmail("admin@admin.com");
+        // drna had l password b dak l l function dyal l hashing
         $admin->setPassword($this->hashPassword($admin, "admin"));
         $admin->setRoles(["ROLE_ADMIN", "ROLE_EDITOR"]);
         $admin->setFirstName("admin");
         $admin->setLastName("admin");
 
+        // glna l doctrine tsjl had entity 3ndha
         $manager->persist($admin);
+        // glna l doctrine tsjl new user f database
         $manager->flush();
     }
 
+    // hashing function
+    // katakhd useer o l password o kadir lih hashing w katrj3o
     public function hashPassword(
         User $user,
         string $plainPassword
